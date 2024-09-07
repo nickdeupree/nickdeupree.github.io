e5d097e3d8e0ac044d939a9ce46d6694589bcf65
Nick Deupree <nicholasdeupree@gmail.com>
2024-09-06 20:35:50 -0400

    prep for pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: 'export',
+    basePath: process.env.NODE_ENV === 'production' ? '/your-repo-name' : '',
+    images: {
+      unoptimized: true,
+    },
+  };
+  
+  export default nextConfig;
