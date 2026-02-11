96bce2099c766f503be29662c505098cd969b5b3
Nick Deupree <nicholasdeupree@gmail.com>
2026-02-10 22:56:32 -0500

    added mcp

--- src/app/data/openSource.ts
+++ src/app/data/openSource.ts
@@ -1,5 +1,12 @@
 export const openSourceProjects = [
   {
+    title: "Lyric-Fetch-MCP",
+    github: "https://github.com/nickdeupree/lyric-fetch-mcp",
+    link: "https://pypi.org/project/lyric-fetch-mcp/",
+    description: "Lyric-Fetch-MCP is an MCP server that allows LLMs to fetch song titles from artists and albums as well as lyrics for specific songs.",
+    difficulty: 2
+  },
+  {
     title: "Expo-alarm-kit",
     github: "https://github.com/nickdeupree/expo-alarm-kit",
     link: "https://www.npmjs.com/package/expo-alarm-kit",
